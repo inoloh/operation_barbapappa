623d8020260330d035bc8d53ccee7702c8036ac6
Henrik Büller <carl.henrik.buller@gmail.com>
2021-05-21 16:05:09 +0200

    <fix> cleaned up main method, now contains only wokring code.

--- src/main/java/se/yrgo/client/Main.java
+++ src/main/java/se/yrgo/client/Main.java
@@ -22,14 +22,14 @@ public class Main {
         jndiProperties.put(Context.PROVIDER_URL, "http-remoting://localhost:8080");
         jndiProperties.put("jboss.naming.client.ejb.context", true);
         jndiProperties.put(Context.SECURITY_AUTHENTICATION, "simple");
-        jndiProperties.put(Context.SECURITY_PRINCIPAL, "appuser3");
-        jndiProperties.put(Context.SECURITY_CREDENTIALS, "password3!");
+        jndiProperties.put(Context.SECURITY_PRINCIPAL, "appuser2");
+        jndiProperties.put(Context.SECURITY_CREDENTIALS, "password2!");
         System.out.println(jndiProperties);
 
         Context jndi = new InitialContext(jndiProperties);
         ZooService service = (ZooService) jndi.lookup("operation_barbapappa-1.0-SNAPSHOT-war/ZooServiceImpl!se.yrgo.service.ZooService");
 
-       /* Animal jeff = new Animal("Jeff", 4, "Hawaiian monk seal", HealthStatus.SICK, "1984-04-12");
+        Animal jeff = new Animal("Jeff", 4, "Hawaiian monk seal", HealthStatus.SICK, "1984-04-12");
         Animal nigel = new Animal("Nigel", 4, "Hawaiian monk seal", HealthStatus.HEALTHY, "1984-04-12");
         Animal esteban = new Animal("Esteban", 4, "Hawaiian monk seal", HealthStatus.SICK, "1984-04-12");
 
@@ -37,33 +37,13 @@ public class Main {
         service.buyAnimal(nigel);
         service.buyAnimal(esteban);
 
-        Zone flood = new Zone("Flood");
-        service.addZone(flood);*/
+        List<Animal> sickAnimals = service.showSickAnimals();
 
-        service.addAnimalToZone(9,12);
-
-        // flood.addAnimals(jeff);
-        // flood.addAnimals(nigel);
-        // flood.addAnimals(esteban);
-
-//        Animal animal = service.getAnimalById(2);
-//        System.out.println("alive");
-//        System.out.println(animal);
-//        service.executeAnimal(3);
- //       service.executeAnimal(5);
-//        System.out.println("dead");x
-
-        List<Animal>sickAnimals = service.showSickAnimals();
-
-
-//        List<Animal> allAnimals = service.showAllAnimals();
         for (Animal animal : sickAnimals) {
             System.out.println(animal);
+
+
         }
-//        List<Animal> ListOfAnimals = service.showAllAnimals();
-//
-//        System.out.println(ListOfAnimals);
-
     }
-}
 
+}
