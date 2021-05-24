e877af1d0a2837515900fe3ea08de78ae907c4eb
Henrik Büller <carl.henrik.buller@gmail.com>
2021-05-24 16:06:33 +0200

    <feat> Added javadoc to classes in dataaccess package.

--- src/main/java/se/yrgo/dataaccess/DataAccess.java
+++ src/main/java/se/yrgo/dataaccess/DataAccess.java
@@ -7,13 +7,55 @@ import se.yrgo.domain.Zone;
 import javax.ejb.Local;
 import java.util.List;
 
+/** Data access layer
+ * Contains methods that communicates directly with the database
+ */
 @Local
 public interface DataAccess {
+
+    /**
+     * Deletes an animal from the database.
+     *
+     * @param animalId the id of the animal to delete
+     * @throws AnimalNotFoundException if animal is not found in database
+     */
     public void deleteAnimal(int animalId) throws AnimalNotFoundException;
+
+    /**
+     *
+     * @param animal object to insert into database
+     * @throws AnimalNotFoundException if animal is not found in database
+     */
     public void insertAnimal(Animal animal) throws AnimalNotFoundException;
+
+    /**
+     *
+     * @return a list of all animals in the database
+     * @throws AnimalNotFoundException if animal is not found in database
+     */
     public List<Animal> findAllAnimals() throws AnimalNotFoundException;
+
+    /**
+     *
+     * @return a list of all sick animals in the database
+     * @throws AnimalNotFoundException if animal is not found in database
+     */
     public List<Animal> findSickAnimals() throws AnimalNotFoundException;
+
+    /**
+     *
+     * @param animalId the id of the animal to find
+     * @return an animal
+     * @throws AnimalNotFoundException if animal is not found in database
+     */
     public Animal findAnimalById(int animalId) throws AnimalNotFoundException;
+
+    /**
+     *
+     * @param animalId the id of the animal to update healthstatus on
+     * @param status the healthstatus to set
+     * @throws HealthNotUpdatedException if animal is not found in database
+     */
     public void updateHealthstatus(int animalId, HealthStatus status) throws HealthNotUpdatedException;
 
 }
