b539838d24d6487216491a52094b606e18628967
Lukas Blomdell <lukasblomdell@gmail.com>
2021-05-24 16:04:53 +0200

    <fix>Moved javadoc from Impl to interface

--- src/main/java/se/yrgo/service/ZooService.java
+++ src/main/java/se/yrgo/service/ZooService.java
@@ -11,11 +11,40 @@ import java.util.List;
 
 @Remote
 public interface ZooService {
+    /** Execute and removes an specific animal.
+     *
+     * @param animalId specified id of the animal.
+     * @throws AnimalNotFoundException if no animal was found with the @param animalId.
+     */
     public void executeAnimal(int animalId) throws AnimalNotFoundException;
+    /** Buys an animal to the Zoo
+     *
+     * @param animal it takes an animal to buy an animal.
+     */
     public void buyAnimal(Animal animal) throws AnimalNotFoundException;
+    /** All the animals at the Zoo.
+     *
+     * @return a list of all the Animals at the Zoo.
+     */
     public List<Animal> showAllAnimals() throws AnimalNotFoundException;
+    /** All the sick animals at the Zoo.
+     *
+     * @return a list of all the sick Animals at the Zoo.
+     */
     public List<Animal> showSickAnimals() throws AnimalNotFoundException;
+    /** Gets an specific animal by id.
+     *
+     * @param animalId specified id of the animal.
+     * @return a animal.
+     * @throws AnimalNotFoundException if no animal was found with the @param animalId.
+     */
     public Animal getAnimalById(int animalId) throws AnimalNotFoundException;
+    /** Change the health status of a specific animal.
+     *
+     * @param animalId specified id of the animal.
+     * @param status specified new health status of the animal (HEALTHY,SICK OR DEAD)
+     * @throws HealthNotUpdatedException if health status is not changed from previous status.
+     */
     public void updateAnimalHealth(int animalId, HealthStatus status) throws HealthNotUpdatedException;
 
 }
